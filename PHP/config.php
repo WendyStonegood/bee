e55d47609968093458ea7511b4b2a6b95976c4b8
Mariah Rockwell <mariahrockwell@mariahs-imac.attlocal.net>
2018-06-25 02:41:51 -0500

    updating

--- PHP/config.php
+++ PHP/config.php
@@ -1,12 +1,12 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'regis');
-define('DB_PASSWORD', 'MSSE');
-define('DB_NAME', 'login');
- 
+define('DB_SERVER', 'us-cdbr-iron-east-04.cleardb.net');
+define('DB_USERNAME', 'b9fdd61686eabb');
+define('DB_PASSWORD', '536c2c77');
+define('DB_NAME', 'heroku_281864c389c4011');
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
