ac75be526b3fc1bf880b3aab79ad6d8635506808
Mariah Rockwell <mariahrockwell@mariahs-imac.attlocal.net>
2018-06-25 02:26:03 -0500

    updating

--- PHP/SearchTwo.php
+++ PHP/SearchTwo.php
@@ -190,7 +190,7 @@ $(document).ready(function(){
  function load_data(query)
  {
   $.ajax({
-   url:"./PHP/dbConnTwo.php",
+   url:"dbConnTwo.php",
    method:"POST",
    data:{query:query},
    success:function(data)
