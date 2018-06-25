f1f7f5cf66e4f3ecc3336399434009f43b98e893
Mariah Rockwell <mariahrockwell@mariahs-imac.attlocal.net>
2018-06-25 02:07:14 -0500

    updating

--- PHP/About.php
+++ PHP/About.php
@@ -5,8 +5,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <link rel="stylesheet" type="text/css" href="./CSS/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="./CSS/BeEvergreenCSS.css">
+  <link rel="stylesheet" type="text/css" href="/CSS/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/CSS/BeEvergreenCSS.css">
   <title>Be Evergreen Template</title>
 
     <style>
